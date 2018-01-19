0b4f13b2e6fdd9971f13d0040212a52a467c7ed2
chanhosum <chanhosum1@gmail.com>
2018-01-19 17:08:36 +0800

    json

--- server.js
+++ server.js
@@ -1,32 +1,41 @@
 var express = require('express')
 var bodyParser = require('body-parser');
-var app = express()
+var app = express();
 var jsonParser = bodyParser.json();
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var mongourl = 'mongodb://ansonanson:ansonanson@ds243325.mlab.com:43325/anson';
+
 
 app.get('/', function(req, res) {
     res.end("hello world vvvv");
-    //res.json({notes: "This is your notebook. Edit this to start saving your notes!"})
 })
 
 app.get('/test', function(req, res) {
     res.end("hello world vvvv test");
-    //res.json({notes: "This is your notebook. Edit this to start saving your notes!"})
 })
 
 app.get('/json', function(req, res) {
     res.json({ "test1": "aaa" });
-    //res.json({notes: "This is your notebook. Edit this to start saving your notes!"})
 })
 
-app.post('/post',jsonParser,function(req, res) {
+app.post('/post', jsonParser, function(req, res) {
     console.log(req.body);
-    res.json(req.body);
-    //res.json({notes: "This is your notebook. Edit this to start saving your notes!"})
+    JSON.stringify(req.body);
+    res.end(JSON.stringify(req.body));
+    //res.json(req.body);
 })
 
-app.get('/db', function(req, res) {
-    res.json({ "test1": "aaa" });
-    //res.json({notes: "This is your notebook. Edit this to start saving your notes!"})
+app.post('/db', jsonParser, function(req, res) {
+    var obj = req.body;
+    MongoClient.connect(mongourl, function(err, db) {
+        switch (obj.operation) {
+            case "insert":
+            	delete obj.operation;
+                db.collection('roadroadtong').insert(obj);
+                break;
+        }
+    });
 })
 
 
