d8cd30d2a4787eaec05559f4a33bb91fb8b404e4
chanhosum <chanhosum1@gmail.com>
2018-01-19 16:40:29 +0800

    json

--- server.js
+++ server.js
@@ -1,21 +1,33 @@
 var express = require('express')
- 
+var bodyParser = require('body-parser');
 var app = express()
- 
+var jsonParser = bodyParser.json();
+
 app.get('/', function(req, res) {
-	res.end("hello world vvvv");
-  //res.json({notes: "This is your notebook. Edit this to start saving your notes!"})
+    res.end("hello world vvvv");
+    //res.json({notes: "This is your notebook. Edit this to start saving your notes!"})
 })
- 
+
 app.get('/test', function(req, res) {
-	res.end("hello world vvvv test");
-  //res.json({notes: "This is your notebook. Edit this to start saving your notes!"})
+    res.end("hello world vvvv test");
+    //res.json({notes: "This is your notebook. Edit this to start saving your notes!"})
 })
 
 app.get('/json', function(req, res) {
-	res.json({"test1":"aaa"});
-  //res.json({notes: "This is your notebook. Edit this to start saving your notes!"})
+    res.json({ "test1": "aaa" });
+    //res.json({notes: "This is your notebook. Edit this to start saving your notes!"})
+})
+
+app.post('/post',jsonParser,function(req, res) {
+    console.log(req.body);
+    res.json(req.body);
+    //res.json({notes: "This is your notebook. Edit this to start saving your notes!"})
+})
+
+app.get('/db', function(req, res) {
+    res.json({ "test1": "aaa" });
+    //res.json({notes: "This is your notebook. Edit this to start saving your notes!"})
 })
 
 
-app.listen(process.env.PORT||8099);
+app.listen(process.env.PORT || 8099);
