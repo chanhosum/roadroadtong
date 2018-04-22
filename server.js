06484509633cbcbc5aee75aee34c1dbd21780113
chanhosum <chanhosum1@gmail.com>
2018-04-22 14:33:36 +0800

    details

--- server.js
+++ server.js
@@ -332,6 +332,7 @@ app.post('/report', jsonParser, function(req, res) {
 });
 
 app.post('/test', jsonParser, function(req, res) {
+    console.log("testttttestttt");
     var obj = req.body;
     console.log(obj);
     res.end("ok");
